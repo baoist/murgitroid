bf5ba2e77b9d5a9559eabd84751f94bac1529f11
Brad Olson <alllateralus@gmail.com>
2011-09-25 02:03:06 -0500

    moved fonts to css dir

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,6 @@ module Murgitroid
     config.filter_parameters += [:password]
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
